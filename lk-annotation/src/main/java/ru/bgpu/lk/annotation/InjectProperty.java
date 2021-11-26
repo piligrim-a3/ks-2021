ccada4e725fe555098235be2858a5561b0aaa36c
ni <piligrim.a3@gmail.com>
2021-11-26 18:31:30 +0900

    add: annotation

---
+++ lk-annotation/src/main/java/ru/bgpu/lk/annotation/InjectProperty.java
@@ -0,0 +1,10 @@
+package ru.bgpu.lk.annotation;
+
+import java.lang.annotation.*;
+
+@Target(value= ElementType.FIELD)
+@Retention(value= RetentionPolicy.RUNTIME)
+public @interface InjectProperty {
+    String name() default "";
+    String defaultValue();
+}
